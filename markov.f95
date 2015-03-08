f8849d8ed8f88b36cf667108d71b57e628d9cfbc
meddlem <mprebergen@gmail.com>
2015-03-08 11:12:14 +0100

    cleaned up markov.f95

--- markov.f95
+++ markov.f95
@@ -11,21 +11,19 @@ contains
     real(dp), intent(out) :: dE
     real(dp), intent(in) :: BJ, h
     integer :: i, j, x(2), S0(L+2,L+2), S_t(L,L)
-    real(dp) :: BF, dE_t, r, u(2)
+    real(dp) :: BF, dE_t, r
     
-    S_t = S
+    S_t = S ! initialize trial config
+    call random_spin(x)
     
-    call random_number(u)
-    u = L*u + 0.5_dp
-    x = nint(u) ! index of spin to flip
     ! create trial config by flipping 1 spin:
     i = x(1); j = x(2)
-    S_t(i,j) = -S_t(i,j) ! flip spin
+    S_t(i,j) = -S_t(i,j)
     
+    ! add zero padding
     S0 = 0
-    S0(2:L+1,2:L+1) = S_t ! add zero padding
-    i = i+1 ! adjust index accordingly
-    j = j+1 
+    S0(2:L+1,2:L+1) = S_t
+    i = i+1; j = j+1 ! adjust indices accordingly 
 
     ! you can just save the possible energy changes, boltzmann factors in &
     ! an array, may save some computation time. could do this in init_energy
@@ -39,7 +37,6 @@ contains
     else ! else accept config with probability of BF
       BF = exp(-dE_t)
       call random_number(r)
-      
       if (r<BF) then
         S = S_t
         dE = dE_t
@@ -48,5 +45,15 @@ contains
         dE = 0._dp
       endif
     endif
+  end subroutine
+
+  subroutine random_spin(x)
+    ! returns index of randomly picked spin
+    integer, intent(out) :: x(:)
+    real(dp) :: u(2)
+
+    call random_number(u)
+    u = L*u + 0.5_dp
+    x = nint(u) ! index of spin to flip
   end subroutine
 end module
