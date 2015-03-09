fadd2bdf6abfb693b1f64a664a8579a1684e2d0f
meddlem <mprebergen@gmail.com>
2015-03-09 18:10:59 +0100

    calculate dE, BF only once, is a bit faster

--- main.f95
+++ main.f95
@@ -12,15 +12,17 @@ program main
   ! h = external field
   ! array containing Spins indexed as row,column
 
-  real(dp) :: BJ, BE, BE_init, dE, h
+  real(dp) :: BJ, BE, BE_init, dE, h, dE_vals(9,2), BF_vals(9,2)
   integer, allocatable :: S(:,:)
-  integer :: i, start_time, end_time, runtime
+  integer :: i, j, start_time, end_time, runtime
   
   allocate(S(L+2,L+2))
   call user_in(BJ,h)
-  ! dE_vals = 2._dp*BJ*(/(i,i=-4,4)/) ! possible values of dE
-  ! BF_vals = exp(-dE_vals) ! possible values of boltzmann factor
 
+  ! calculate values of dE and boltzmann factor 
+  forall(i=1:9,j=1:2) dE_vals(i,j) = - 2._dp*BJ*(i-5) - 2._dp*h*(j*2-3) 
+  BF_vals = exp(-dE_vals)
+  
   call init_random_seed()
   call init_lattice(S)
   call init_energy(BE,S,BJ,h)
@@ -30,7 +32,7 @@ program main
  
   call system_clock(start_time)
   do i=1,100*N
-    call gen_config(S,dE,BJ,h)
+    call gen_config(S,dE,dE_vals,BF_vals)
     BE = BE + dE
   enddo
   call system_clock(end_time)
